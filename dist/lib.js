94c81a19c3d147614148305f9f874341b3c847e7
Ali Mohsin <alimohsin.pro@gmail.com>
2022-05-30 04:39:15 +0600

    updates

--- dist/lib.js
+++ dist/lib.js
@@ -1,29 +1,59 @@
 var doc=document;
 var dom=document.querySelector.bind(document);
 var doms=document.querySelectorAll.bind(document);
-HTMLElement.prototype.dom=HTMLElement.prototype.querySelector;
-HTMLElement.prototype.doms=HTMLElement.prototype.querySelectorAll;
+Element.prototype.dom=Element.prototype.querySelector;
+Element.prototype.doms=Element.prototype.querySelectorAll;
+var $=document.querySelector.bind(document);
+var $$=document.querySelectorAll.bind(document);
+Element.prototype.$=Element.prototype.querySelector;
+Element.prototype.$$=Element.prototype.querySelectorAll;
+// Event
 EventTarget.prototype.on = EventTarget.prototype.addEventListener;
-EventTarget.prototype.trigger = EventTarget.prototype.dispatchEvent;
-HTMLElement.prototype.hasAttr=HTMLElement.prototype.hasAttribute;
-HTMLElement.prototype._setAttribute=HTMLElement.prototype.setAttribute;
-HTMLElement.prototype.setAttribute=function(name,value){
-	var oldValue=this.getAttribute(name);
-	this._setAttribute(name,value);
-	this.dispatchEvent(new CustomEvent('attrChange',{detail:{
-		name:name,value:value,oldValue:oldValue
-	}}));
+EventTarget.prototype.off = EventTarget.prototype.removeEventListener;
+// EventTarget.prototype.dispatch = EventTarget.prototype.dispatchEvent;
+// EventTarget.prototype.trigger = EventTarget.prototype.dispatchEvent;
+EventTarget.prototype['@on']=function(name,detail){
+	this.dispatchEvent(new CustomEvent(name,{detail:detail}));
 };
-HTMLElement.prototype.attr=function(){
-return arguments.length==1 && this.getAttribute(arguments[0])
-	|| arguments.length==2 && this.setAttribute(arguments[0],arguments[1]);
+// Attribute
+Element.prototype.hasAttr=Element.prototype.hasAttribute;
+Element.prototype.removeAttr=Element.prototype.removeAttribute;
+Element.prototype.attr=function(){
+	if(arguments.length==1){return this.getAttribute(arguments[0])}
+	if(arguments.length>1){this.setAttribute(arguments[0],arguments[1])}
 };
-function forEach(obj,func){Array.prototype.forEach.call(obj,func)}
+Element.prototype['+attr']=function(attr){this.setAttribute(attr,'')};
+Element.prototype['-attr']=function(attr){this.removeAttribute(attr)};
+Element.prototype['?attr']=function(attr){return this.hasAttribute(attr)};
+Element.prototype['@attr']=function(attr){return this.getAttribute(attr)};
+Element.prototype['=attr']=function(attr){return this.getAttribute(attr)};
+Element.prototype['attr=']=function(attr,val){this.setAttribute(attr,val)};
+// Class
+Element.prototype['+cls']=function(cls){this.classList.add(cls)};
+Element.prototype['-cls']=function(cls){this.classList.remove(cls)};
+Element.prototype['?cls']=function(cls){return this.classList.contains(cls)};
+// HTML
+Element.prototype._innerHTML=Object.getOwnPropertyDescriptor(Element.prototype,'innerHTML');
+Object.defineProperty(Element.prototype,'html',Element.prototype._innerHTML);
+// Element.prototype['+html']=function(html){this.insertAdjacentHTML('afterbegin',html)}
+// Element.prototype['html+']=function(html){this.insertAdjacentHTML('beforeend',html)}
+Object.defineProperty(Element.prototype,'+html',{
+	set(html){this.insertAdjacentHTML('afterbegin',html)}
+});
+Object.defineProperty(Element.prototype,'html+',{
+	set(html){this.insertAdjacentHTML('beforeend',html)}
+});
+// Each Loop
 NodeList.prototype.each=NodeList.prototype.forEach;
 Array.prototype.each=Array.prototype.forEach;
 Object.prototype.each=function(func){obj=this;
 	for(const key in obj){if(obj.hasOwnProperty(key)&&func){func(obj[key],key,obj)}}
 };
+// css
+Element.prototype.css=function(css){that=this;
+	css.each(function(val,prop){that.style.setProperty(prop,val)});
+};
+// Template
 String.prototype.render=function(data){return this.replace(/\${(.*?)}/g,function(x,g){
 	return g.split(/[\.\[\]\'\"]/g).filter(Boolean).reduce(function(obj,i){return obj[i]},data)
 })};
