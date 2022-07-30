2d91b634d315a003354367e217f3a1b2b4066fad
Ali Mohsin <alimohsin.pro@gmail.com>
2022-07-30 15:45:30 +0600

    clean

--- src/dev.js
+++
@@ -1,7 +0,0 @@
-Function.prototype.test=function(name,loop){
-	console.time(name);for(var i=0;i<loop;i++){this()}console.timeEnd(name);
-};
-
-const test=function(name,loop,fn){
-	console.time(name);for(var i=0;i<loop;i++){fn()}console.timeEnd(name);
-};
